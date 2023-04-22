4f02625ca7443e41e162e8db49ccdc44f7e01c52
S00244767 <S00244767@atu.ie>
2023-04-22 17:24:14 +0100

    Ajoutez des fichiers projet.

---
+++ InsuranceService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public interface IDiscountService
+    {
+        double GetDiscount();
+    }
+    public class InsuranceService
+    {
+        private readonly IDiscountService _discountInterface;
+        public InsuranceService(IDiscountService discountInterface)
+        {
+            _discountInterface = discountInterface;
+        }
+
+        public double CalcPremium(int age, string location)
+        {
+            double premium;
+
+            if (location == "rural")
+            {
+                if ((age >= 18) && (age < 30))
+                    premium = 5.0;
+                else if (age >= 31)
+                    premium = 2.50;
+                else
+                    premium = 0.0;
+            }
+            else if (location == "urban")
+            {
+                if ((age >= 18) && (age <= 35))
+                    premium = 6.0;
+                else if (age >= 36)
+                    premium = 5.0;
+                else
+                    premium = 0.0;
+            }
+            else
+                premium = 0.0;
+
+            double discount = _discountInterface.GetDiscount();
+            if (age >= 50)
+                premium = premium * discount;
+            return premium;
+        }
+    }
+
+}
